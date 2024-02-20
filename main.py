e127904ac25cfb614a97ad8f9df87e4b58e7b633
Adsekiii <149227913+Adsekiii@users.noreply.github.com>
2024-02-20 19:18:27 +0100

    Tutorial Files
    Added files that I created while following the tutorial

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+class MyApp(App):
+    def build(self):
+        pass
+MyApp().run()
+
