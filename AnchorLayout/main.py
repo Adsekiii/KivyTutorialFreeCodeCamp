4c96a01caffc85a5c4249d0060fcaf5d5aab1ea1
Adsekiii <149227913+Adsekiii@users.noreply.github.com>
2024-02-21 19:49:21 +0100

    Added files AnchorLayout files
    added AnchorLayout folder inside of which there are files containing AnchorLayout in Kivy

---
+++ AnchorLayout/main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+ 
+class MyApp(App):
+    def build(self):
+        pass
+MyApp().run()
