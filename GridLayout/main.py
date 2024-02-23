3958549df901c2c56af51cb0fe30247bde16f9a5
Adsekiii <149227913+Adsekiii@users.noreply.github.com>
2024-02-23 21:36:02 +0100

    Grid Layout
    I've done some grid layouts and settled on this one, marking the end for now regarding grid layout in Kivy

---
+++ GridLayout/main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class MyApp(App):
+    def build(self):
+        pass
+
+class GridLayoutExample(GridLayout):
+    pass
+
+def main():
+    MyApp().run()
+
+
+if __name__ == "__main__":
+    main()
