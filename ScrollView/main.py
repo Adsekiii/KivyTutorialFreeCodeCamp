900b3715914057cd5ee8997f92b4075a0690e433
Adsekiii <149227913+Adsekiii@users.noreply.github.com>
2024-02-26 13:52:18 +0100

    ScrollView
    Added the ability to scroll inside the app as well as set the height to the actuall needed size

---
+++ ScrollView/main.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.button import Button
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        size = '120dp'
+        # self.orientation = "lr-bt"
+        # self.padding = '60dp', '60dp'
+        # self.spacing = '60dp', '60dp'
+        for i in range(100):
+            b = Button(text=str(i+1),size_hint=(None,None),size=(size,size))
+            self.add_widget(b)
+
+class MyApp(App):
+    def build(self):
+        self.title = "ScrollView"
+        
+        
+    
+    
+MyApp().run()
